bff2066b5025018dada70d356c10a2ee6c43d622
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-14 01:09:04 +0100

    Initial Commit

---
+++ TikTok-SwiftUI/TikTok_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_SwiftUIApp.swift
+//  TikTok-SwiftUI
+//
+//  Created by Khan on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
