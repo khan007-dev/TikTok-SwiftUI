6562b76df3339133ad030f58472b2436555c47b2
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-15 14:51:54 +0100

    Tab Bar setup

--- TikTok-SwiftUI/Core/TabBar/MainTabView.swift
+++ TikTok-SwiftUI/Core/TabBar/MainTabView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State private var selectedTab = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView (selection: $selectedTab) {
+         
+         
+            Text("Feed")
+                .tabItem {
+                    VStack{
+                        Image(systemName: selectedTab == 0 ? "house.fill": "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear() { selectedTab = 0}
+                .tag(0)
+            
+            Text("Friends")
+                .tabItem {
+                    VStack{
+                        Image(systemName: selectedTab == 1 ?  "person.2.fill" : "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear() { selectedTab = 1}
+                .tag(1)
+            
+            Text("Feed")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "plus.app.fill")
+                            .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                        
+                    }
+                }.onAppear() { selectedTab = 2}
+                .tag(2)
+            
+            Text("Notification")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "heart")
+                           
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Home")
+                    }
+                }.onAppear() { selectedTab = 3}
+                .tag(3)
+            
+            
+            Text("Profile")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }.onAppear() { selectedTab = 4}
+                .tag(4)
+        }
     }
 }
 
