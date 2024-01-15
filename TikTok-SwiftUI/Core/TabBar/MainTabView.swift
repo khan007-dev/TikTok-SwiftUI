d460518b990dd75f8e69d4922d89974025084d86
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-15 14:51:33 +0100

    Tab Bar setup

---
+++ TikTok-SwiftUI/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TikTok-SwiftUI
+//
+//  Created by Khan on 15.01.2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
