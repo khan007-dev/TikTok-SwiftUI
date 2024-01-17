4608cf7faa2dbd14b1a6764d355710fa3c763d22
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-17 08:00:42 +0100

    FeedView

---
+++ TikTok-SwiftUI/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  TikTok-SwiftUI
+//
+//  Created by Khan on 17.01.2024.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
