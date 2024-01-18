48c38ddaa25770775fc2cdf545930f7c2a7e57dd
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-18 23:45:28 +0100

    Explore View

---
+++ TikTok-SwiftUI/Core/Search/ExplorerView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplorerView.swift
+//  TikTok-SwiftUI
+//
+//  Created by Khan on 18.01.2024.
+//
+
+import SwiftUI
+
+struct ExplorerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplorerView()
+}
