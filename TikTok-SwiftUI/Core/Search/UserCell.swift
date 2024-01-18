a6398bfb98b7223a0ff64555c82cb971d9f30226
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-18 23:45:39 +0100

    Explore View

--- TikTok-SwiftUI/Core/Search/UserCell.swift
+++ TikTok-SwiftUI/Core/Search/UserCell.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct UserCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(Color(.systemGray5))
+            
+            VStack {
+                Text("Test User")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Test Name")
+                    .font(.footnote)
+            }
+            
+            Spacer()
+        }
     }
 }
 
