2f17e723eccbecf64cf258ed971829fc9a5cca69
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-18 23:48:54 +0100

    Explore View

--- TikTok-SwiftUI/Core/Search/ExplorerView.swift
+++ TikTok-SwiftUI/Core/Search/ExplorerView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ExplorerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        ScrollView {
+          
+            LazyVStack (spacing: 16) {
+                ForEach(0 ..< 20) { data in
+                
+                    UserCell()
+                        .padding(.horizontal)
+                }
+            }
+        }
     }
 }
 
