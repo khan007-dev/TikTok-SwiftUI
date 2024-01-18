31707a29e21eea5bbd618e2de54f7a0f14a15cad
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-19 00:13:11 +0100

    Notifications View

---
+++ TikTok-SwiftUI/Core/Notification/NotificationCell.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationCell.swift
+//  TikTok-SwiftUI
+//
+//  Created by Khan on 19.01.2024.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationCell()
+}
---
+++ TikTok-SwiftUI/Core/Notification/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  TikTok-SwiftUI
+//
+//  Created by Khan on 19.01.2024.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
